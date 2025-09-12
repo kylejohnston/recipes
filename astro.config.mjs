4cbc752f6225d405e8730742f199338d22d25a2d
Kyle Johnston <flow14@gmail.com>
2025-09-11 23:43:08 -0500

    feat: migrate to astro v5 and tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://softpanda.de/',
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   // enable this for local development
   // server: { host: true },
 });
